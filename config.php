6654ef2728236082ea1de7cb3bc840b8e0734498
Ruman Saleem <ruman63@gmail.com>
2018-10-16 01:30:23 +0530

    add config for blog posts

--- config.php
+++ config.php
@@ -5,10 +5,18 @@ return [
         'destination' => 'dist',
     ],
     'baseUrl' => '',
+    'active' => function ($page, $selection) {
+        return str_contains($page->getPath(), $selection) ? 'active' : '';
+    },
+    'author' => 'Ruman Saleem',
+    'website' => 'http://rummansaleem.me',
     'production' => false,
     'collections' => [
         'skills' => [
             'sort' => 'order',
+        ],
+        'posts' => [
+            'path' => 'blog/{filename}',
         ],
     ],
 ];
