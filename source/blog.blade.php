d5272be69c4cff6ef167f060d73413b723144349
Ruman Saleem <ruman63@gmail.com>
2018-10-16 01:22:58 +0530

    Add Blog Page

---
+++ source/blog.blade.php
@@ -0,0 +1,8 @@
+@extends('_layouts.master')
+@section('main-content')
+
+<div class="flex justify-center items-center min-h-screen">
+    <h2 class="text-grey font-serif">Arriving Soon...</h2>
+</div>
+
+@endsection
